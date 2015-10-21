e81537efd46a0f55522b3159b891c789b681b156
Hackbright Student <info@hackbrightacademy.com>
2015-10-20 17:55:47 -0700

    Added login

--- model.py
+++ model.py
@@ -52,9 +52,17 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     score = db.Column(db.Integer, nullable=False)
+
+      # Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("ratings", order_by=rating_id))
+
+     # Define relationship to movie
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings", order_by=rating_id))
 
     def __repr__(self):
         """ Provide helpful representation when printed."""
--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,7 +22,60 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+
+@app.route('/users')
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route('/users/<int: user.user_id')
+def user_details():
+    """Shows detail;s about a user clicked on, including age, zipcode, the list 
+    of movies that they reviewed, and their ratings for those movies."""
+
+    user = User.query.get(user.user_id).first() #gets the object associated with the user
+    user_age = user.age
+    user_zipcode = user.zipcode
+    user_ratings = user.ratings
+    print user_ratings
+
+    return redirect("/")
+
+
+@app.route('/login')
+def show_login_form():
+    """Shows login form."""
+
+    return render_template("login.html")
+
+
+@app.route('/process_login', methods=["POST"])
+def process_login():
+    """Checks if the user exists in the User table. If not, creates a new user."""
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    user_object = User.query.filter(User.email == username).first()
+
+    if not user_object:
+        new_user = User(email=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+    else:
+        user_email= user_object.email
+        user_password = user_object.password
+        if user_password != password:
+            return redirect("/login")
+        else:
+            session['user_name'] = user_email
+    return redirect("/")
+
+
+
 
 
 if __name__ == "__main__":
